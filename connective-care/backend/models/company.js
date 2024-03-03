45df004d17eecb8a106e252166e4238566a7a0d5
Accu <matthewsoto85@gmail.com>
2024-03-02 20:02:58 -0500

    restructured company format to be correct

--- connective-care/backend/models/company.js
+++ connective-care/backend/models/company.js
@@ -13,30 +13,38 @@ const driverSchema = new mongoose.Schema({
 
 // company db schemas
 const companySchema = new mongoose.Schema({
-    name: {
+  name: {
       type: String,
       required: true
-    },
-    registrationInfo: {
-      insurance: String,
-      businessEIN: String,
-      address: String,
-      required:true
-    },
-    account: {
+  },
+  registrationInfo: {
+      insurance: {
+          type: String,
+          required: true
+      },
+      businessEIN: {
+          type: String,
+          required: true
+      },
+      address: {
+          type: String,
+          required: true
+      }
+  },
+  account: {
       username: {
-        type: String,
-        required: true,
-        unique: true
+          type: String,
+          required: true,
+          unique: true
       },
       password: {
-        type: String,
-        required: true
+          type: String,
+          required: true
       }
-    },
-    drivers: [driverSchema],
-    // Add other company-specific fields as needed
-  });
+  },
+  drivers: [driverSchema]
+  // Add other company-specific fields as needed
+});
   
  
   
