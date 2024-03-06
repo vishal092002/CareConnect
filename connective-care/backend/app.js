397a41c59133692ebf904d2b2a81ddf0d8eab1dc
Matthew Barrera <mub6@njit.edu>
2024-03-06 15:42:33 -0500

    Created connection between web app and MongoDB Atlas

--- connective-care/backend/app.js
+++ connective-care/backend/app.js
@@ -12,7 +12,35 @@ app.use(express.json())
 
 
 // mondgodb account and db needs to be made for this to function properly, when made add it to an env folder so we can utilize it
-mongoose.connect()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// Actually URI is stored elsewhere
+// const uri = process.env.MONGOLAB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // After we run "await client.connect()", we're in! Biw we cab read abd wrute data.
+    // My recommendation would be to do something like --> const userCollection = await client.db().collection('Accounts.Users');
+    // Same idea for 'Accounts.Providers'
+
+    // It looks like this runs until (Finally) the client closes
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 //routes
