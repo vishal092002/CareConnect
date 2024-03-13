04231464ef21ed00838a877425e8ffba9a2a4e92
meghana221997 <meghana.kpm@gmail.com>
2024-03-13 14:41:20 -0400

    API data

---
+++ connective-care/src/data/driverData.js
@@ -0,0 +1,57 @@
+export const drivers = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      profilePicture: 'a.jpg',
+      companyName: 'ABC Taxi',
+      idNumber: '12345'
+    },
+    {
+      id: 2,
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      profilePicture: 'b.jpg',
+      companyName: 'XYZ Rides',
+      idNumber: '67890'
+    }
+  ];
+  
+  export const driverAides = [
+    {
+      id: 1,
+      firstName: 'Jane',
+      lastName: 'Smith',
+      profilePicture: 'c.jpg',
+      companyName: 'ABC Taxi',
+      idNumber: '54321'
+    },
+    {
+      id: 2,
+      firstName: 'Bob',
+      lastName: 'Johnson',
+      profilePicture: 'd.jpg',
+      companyName: 'XYZ Rides',
+      idNumber: '09876'
+    },
+  ];
+  
+  export const displayAllDrivers = (drivers) => {
+    console.log("All Drivers:");
+    drivers.forEach(driver => {
+      console.log(`
+        Name: ${driver.firstName} ${driver.lastName}
+        Company: ${driver.companyName}
+        ID: ${driver.idNumber}
+      `);
+    });
+  };
+  
+  export const filterDriversByCompany = (companyName) => {
+    return drivers.filter(driver => driver.companyName === companyName);
+  };
+  
+  export const filterDriverAidesByCompany = (companyName) => {
+    return driverAides.filter(driverAide => driverAide.companyName === companyName);
+  };
+  
